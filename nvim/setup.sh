4dff3a098cb020d8c28d2b0c415ab084a8643cfc
James Brock <jamesbrock@gmail.com>
2019-05-21 14:39:56 +0900

    clone Vundle

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
+
 mkdir -p ~/.config/nvim
 cp --no-clobber init.vim ~/.config/nvim/
-ln -s ${PWD}/init.basic.vim ~/.config/nvim/init.basic.vim
-ln -s ${PWD}/init.plugins.vim ~/.config/nvim/init.plugins.vim
+ln -f -s ${PWD}/init.basic.vim ~/.config/nvim/init.basic.vim
+ln -f -s ${PWD}/init.plugins.vim ~/.config/nvim/init.plugins.vim
+mkdir -p ~/.vim/bundle/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 
