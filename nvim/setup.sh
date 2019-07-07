4a9309324d701971787e2d0333d6713a0fe72d31
James Brock <jamesbrock@gmail.com>
2019-07-07 16:59:16 +0900

    setup vim-plug

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -6,7 +6,6 @@ mkdir -p ~/.config/nvim
 cp --no-clobber init.vim ~/.config/nvim/
 ln -f -s ${PWD}/init.basic.vim ~/.config/nvim/init.basic.vim
 ln -f -s ${PWD}/init.plugins.vim ~/.config/nvim/init.plugins.vim
-mkdir -p ~/.vim/bundle/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
