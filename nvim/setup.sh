c3e476b8147d4cf891feb80a71854cc9aa6618c8
James Brock <jamesbrock@gmail.com>
2019-12-24 16:55:45 +0900

    Run the nvim PlugInstall command

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -9,3 +9,4 @@ ln -f -s ${PWD}/init.plugins.vim ~/.config/nvim/init.plugins.vim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+nvim +PlugInstall +qall
